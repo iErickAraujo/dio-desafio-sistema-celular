0b9e50f6d4dad302f3735d867ac9a9f02ffbba6b
Erick Araujo <erickaraujo4984@gmail.com>
2024-01-08 19:13:34 -0300

    Projeto funcional
    Signed-off-by: Erick Araujo <erickaraujo4984@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Nokia
+Console.WriteLine("Criando Nokia...");
+Nokia meuNokia = new Nokia("99999999", "Nokia Tijolão", "111111111111111", 128);
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+//Iphone
+Console.WriteLine("Criando Iphone...");
+Iphone meuIphone = new Iphone("70707070", "Iphone Brastemp", "222222222222222", 256);
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("Instagram");
